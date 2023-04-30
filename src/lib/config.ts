5c5dd7ebac17618694ac9ebcec0baed31f90f762
Aditya Ghidora <aditya@ghidora.tech>
2023-04-30 12:02:40 +0530

    added header and footer

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
 
-export const title = 'Shakespear';
+export const title = 'Shakespeare';
 export const description = 'SvelteKit blog for poets';
 export const url = dev ? 'http://localhost:5173' : 'real_url';
---
+++ src/routes/Header.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+</script>
+
+<nav>
+	<a href="/" class="title"><b>{config.title}</b></a>
+
+	<ul class="links">
+		<li><a href="/about">about</a></li>
+		<li><a href="/contact">contact</a></li>
+		<li><a href="/rss.xml" target="_blank">RSS</a></li>
+	</ul>
+
+	<button>Toggle</button>
+</nav>
+
+<style>
+	nav {
+		padding-block: var(--size-7);
+	}
+	.links {
+		margin-block: var(--size-7);
+	}
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+	@media (min-width: 768px) {
+		nav {
+			display: flex;
+			justify-content: space-between;
+		}
+		.links {
+			display: flex;
+			gap: var(--size-7);
+		}
+	}
+</style>

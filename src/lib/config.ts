de1e931ebbc77a2b0b2042154c56039c67b4af14
Aditya Ghidora <aditya@ghidora.tech>
2023-04-30 11:15:32 +0530

    starter project added

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Shakespear';
+export const description = 'SvelteKit blog for poets';
+export const url = dev ? 'http://localhost:5173' : 'real_url';
